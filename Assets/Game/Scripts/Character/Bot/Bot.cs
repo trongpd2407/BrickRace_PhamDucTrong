bd0bcbe7da61e9b77678aa58ddcd04061250dcbc
trongpd2407 <trongpd2407@gmail.com>
2024-07-30 17:57:09 +0700

    ~Fix Bot, + Door, ~ Update Bridge,Stage

--- Assets/Game/Scripts/Character/Bot/Bot.cs
+++ Assets/Game/Scripts/Character/Bot/Bot.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.EventSystems;
@@ -13,21 +15,48 @@ public class Bot : AbstractCharacter
     [SerializeField]
     private NavMeshAgent agent;
 
- 
+    private List<Transform> listStages;
+
+    private int currentStage;
+    private int nextStage;
+
+    private bool isFindBridge;
+
+    private Vector3 targetBridge;
+
+    [SerializeField] private int ATTACK_PLAYER_THRESHOLD;
+    [SerializeField] private int BUILD_BRIDGE_THRESHOLD;
+
     private void Start()
     {
-        if(currentState == null) {
+        listStages = new List<Transform>();
+        GameObject level = GameObject.Find("Level");
+        if (level == null) {
+            Debug.LogError("Can not find level");
+        }
+        Transform stageTf = level.transform.Find("Stage");
+        if (stageTf == null)
+        {
+            Debug.LogError("CanNotFindStage");
+        }
+        foreach (Transform child in stageTf)
+        {
+            listStages.Add(child);
+        }
+        currentStage = 0;
+
+        if (currentState == null) {
             ChangeState(new PickBrickState());
         }
     }
     private void Update()
     {
         currentState.OnExecute(this);
-      
     }
     private void FixedUpdate()
     {
     }
+
     public void ChangeState(IState<Bot> state)
     {
         if (currentState != null)
@@ -51,23 +80,85 @@ public class Bot : AbstractCharacter
         target = newTarget;
     }
 
-  
-    public void PickBrick()
+
+    public void FindBrick()
     {
+        if (Vector3.Distance(transform.position, listStages[currentStage].position) > 1.3f)
+        {
+            SetTarget(listStages[currentStage].position);
+        }
         Collider[] colliders = new Collider[5];
-        int numCollider = Physics.OverlapSphereNonAlloc(transform.position, 2f, colliders, layerMask) ;
+        int numCollider = Physics.OverlapSphereNonAlloc(transform.position, 2f, colliders, layerMask);
         float minDis = float.MaxValue;
-        for(int i = 0; i < numCollider; i++)
+        for (int i = 0; i < numCollider; i++)
         {
             float distance = Vector3.Distance(colliders[i].transform.position, transform.position);
-            if ( distance < minDis)
+            if (distance < minDis)
             {
                 minDis = distance;
                 SetTarget(colliders[i].transform.position);
             }
         }
     }
+    public void IncreaseStage()
+    {
+        currentStage++;
+    }
+    public void SetNextStage()
+    {
+        nextStage = currentStage + 1;
+    }
+    public Transform GetNextStage()
+    {
+        return listStages[nextStage];
+    }
+    public Vector3 GetTarget()
+    {
+        return target;
+    }
+    public Vector3 GetTargetBridge()
+    {
+        return targetBridge;
+    }
 
+    public int GetAttackThreshold()
+    {
+        return ATTACK_PLAYER_THRESHOLD;
+    }
+
+    public int GetBuildBridgeThreshold()
+    {
+        return BUILD_BRIDGE_THRESHOLD;
+    }
+
+    public void SetIsFindBridge(bool isFind)
+    {
+        isFindBridge = isFind;
+    }
+    public void FindBridge(Transform stage)
+    {
+        if (!isFindBridge)
+        {
+            Debug.Log(stage.name);
+            float minDis = float.MaxValue;
+            Transform bridge = stage.Find("Bridge");
+            if (bridge == null)
+            {
+                Debug.LogError("Cant Find Bridge");
+            }
+            foreach (Transform bridgeChild in bridge)
+            {
+                float distance = Vector3.Distance(bridgeChild.position, transform.position);
+                if (distance < minDis)
+                {
+                    minDis = distance;
+                    targetBridge = bridgeChild.position;
+                    isFindBridge = true;
+                }
+            }
+        }
+        SetTarget(targetBridge);
+    }
  
 }
 
---
+++ Assets/Game/Scripts/Door/Door.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private Transform leftDoor;
+    [SerializeField] private Transform rightDoor;
+    [SerializeField] private BoxCollider boxCollider;
+
+    public void OnOpen()
+    {
+        leftDoor.position = Vector3.MoveTowards(leftDoor.position,new Vector3(leftDoor.position.x - 0.3f,leftDoor.position.y, leftDoor.position.z), 0.5f);
+        rightDoor.position = Vector3.MoveTowards(rightDoor.position, new Vector3(rightDoor.position.x + 0.3f, rightDoor.position.y, rightDoor.position.z), 0.5f);
+    }
+
+    public void OnClose() 
+    {
+        leftDoor.position = Vector3.MoveTowards(leftDoor.position, new Vector3(leftDoor.position.x + 0.3f, leftDoor.position.y, leftDoor.position.z), 0.5f);
+        rightDoor.position = Vector3.MoveTowards(rightDoor.position, new Vector3(rightDoor.position.x - 0.3f, rightDoor.position.y, rightDoor.position.z), 0.5f);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        OnOpen();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Bot"))
+        {
+            Bot bot = other.GetComponent<Bot>();
+            bot.IncreaseStage();
+            bot.SetIsFindBridge(false);
+            bot.ChangeState(new PickBrickState());
+        }
+        OnClose();
+    }
+}
